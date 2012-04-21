516445c93dee0af39bd63b080b92d17c25ce6344
Panggi Libersa Jasri Akadol <panggi@sukeru.com>
2012-04-21 23:02:17 +0700

    rails empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelImpact::Application.config.secret_token = 'be5f89ba6770d8a4588aac7f85b389c8621c4ec55f34f3003d04f37dfdeadb6e8c87b47fdae3ae9ad39d5ddbc973b824fa9f51d8a4d369777e530122c82b4037'
